af8efc422aefcce041a9a6ab354efd41ee1144e9
codeonjava <mmanandhar@bitscrafters.com>
2014-11-18 18:45:15 +0545

    commit new class

--- src/main/java/com/chetan/cg/Main.java
+++ src/main/java/com/chetan/cg/Main.java
@@ -2,6 +2,6 @@ package com.chetan.cg;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("This is a test file");
+		System.out.println("This is a test file1");
 	}
 }
---
+++ src/main/java/com/chetan/cg/TestClass1.java
@@ -0,0 +1,5 @@
+package com.chetan.cg;
+
+public class TestClass1 {
+// this is a test class
+}
