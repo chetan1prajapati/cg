4468247ff9668123fe3bf5cf340257deb587adc6
chetan <chetan1prajapati@gmail.com>
2014-11-18 19:45:12 +0545

    fake if

--- src/main/java/com/chetan/cg/Main.java
+++ src/main/java/com/chetan/cg/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		//comments
+		if(true)
 		System.out.println("asdfasdf");
 		System.out.println("This 1 is a test file100");
 	//comments more
