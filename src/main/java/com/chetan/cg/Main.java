3ce8744bcb25f3f583d1e17e27a618ff76729f11
codeonjava <mmanandhar@bitscrafters.com>
2014-11-18 19:49:44 +0545

    asdf asdfa jpt for loop

--- src/main/java/com/chetan/cg/Main.java
+++ src/main/java/com/chetan/cg/Main.java
@@ -1,13 +1,15 @@
 package com.chetan.cg;
 
 public class Main {
+
 	public static void main(String[] args) {
 
-		//comments
-		System.out.println("asdfasdf");
-		System.out.println();
+		// comments
+		for (int i = 1; i < 10; i++)
+			System.out.println("asdfasdfasdf asdfa afdasdfasdf");
+		System.out.println("sadfasdfa asd fas dfasdfasdf");
 		System.out.println("This 1 is a test file100");
-	//comments more
+		// comments more
 
 	}
 }
