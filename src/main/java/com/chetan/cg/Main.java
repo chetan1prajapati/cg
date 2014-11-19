c4891d26d21a4e0d113a13dc845729d18b028001
Sujan <sthapa@bitscrafters.com>
2014-11-19 09:46:03 +0545

    Sujan changes

--- src/main/java/com/chetan/cg/Main.java
+++ src/main/java/com/chetan/cg/Main.java
@@ -4,4 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("This is a test file");
 	}
+
+	public void isNajus() {
+		System.out.println("This is Sujan.");
+	}
 }
